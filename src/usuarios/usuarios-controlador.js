828e87fa9f5802e1bee9ffe4f6bf57fa969b912c
João Lourenço <joaodslourenco@gmail.com>
2022-07-21 14:16:02 -0300

    feat: jwt token implemented

--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -1,5 +1,15 @@
 const Usuario = require("./usuarios-modelo");
 const { InvalidArgumentError, InternalServerError } = require("../erros");
+const jwt = require("jsonwebtoken");
+
+function criaTokenJWT(usuario) {
+  const payload = {
+    id: usuario.id,
+  };
+  const token = jwt.sign(payload, process.env.SECRET_KEY);
+  console.log(process.env.SECRET_KEY);
+  return token;
+}
 
 module.exports = {
   adiciona: async (req, res) => {
@@ -26,6 +36,8 @@ module.exports = {
   },
 
   login: (req, res) => {
+    const token = criaTokenJWT(req.user);
+    res.set("Authorization", token);
     res.status(204).send();
   },
 
