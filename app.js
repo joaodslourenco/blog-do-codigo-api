ad93e6038df3d73d2e7c4b9c41e6fc8a837ed743
João Lourenço <joaodslourenco@gmail.com>
2022-07-21 11:16:48 -0300

    feat: login implemented

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const { estrategiasAutenticacao } = require("./src/usuarios");
 
 app.use(
   bodyParser.urlencoded({
-    extended: true
-  })
+    extended: true,
+  }),
 );
 
 module.exports = app;
