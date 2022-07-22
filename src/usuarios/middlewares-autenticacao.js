29ad22d9f84e5b27bb09a1eea00d62ec85b29e18
João Lourenço <joaodslourenco@gmail.com>
2022-07-22 10:00:13 -0300

    feat: middleware to check user token

--- src/usuarios/middlewares-autenticacao.js
+++ src/usuarios/middlewares-autenticacao.js
@@ -21,4 +21,23 @@ module.exports = {
       },
     )(req, res, next);
   },
+  bearer: (req, res, next) => {
+    passport.authenticate(
+      "bearer",
+      { session: false },
+      (error, usuario, info) => {
+        if (error && error.name === "JsonWebTokenError") {
+          return res.status(401).json({ error: error.message });
+        }
+        if (error) {
+          return res.status(500).json({ error: error.message });
+        }
+        if (!usuario) {
+          return res.status(401).json();
+        }
+        req.user = usuario;
+        return next();
+      },
+    )(req, res, next);
+  },
 };
--- src/usuarios/usuarios-rotas.js
+++ src/usuarios/usuarios-rotas.js
@@ -1,5 +1,4 @@
 const usuariosControlador = require("./usuarios-controlador");
-const passport = require("passport");
 const middlewaresAutenticacao = require("./middlewares-autenticacao");
 
 module.exports = (app) => {
@@ -14,8 +13,5 @@ module.exports = (app) => {
 
   app
     .route("/usuario/:id")
-    .delete(
-      passport.authenticate("bearer", { session: false }),
-      usuariosControlador.deleta,
-    );
+    .delete(middlewaresAutenticacao.bearer, usuariosControlador.deleta);
 };
