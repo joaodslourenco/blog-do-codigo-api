59791ef74d4a8f0330c2fec49d96d877b8920381
João Lourenço <joaodslourenco@gmail.com>
2022-07-22 11:20:22 -0300

    feat: error message added to expired token

--- src/usuarios/middlewares-autenticacao.js
+++ src/usuarios/middlewares-autenticacao.js
@@ -29,6 +29,11 @@ module.exports = {
         if (error && error.name === "JsonWebTokenError") {
           return res.status(401).json({ error: error.message });
         }
+        if (error && error.name === "TokenExpiredError") {
+          return res
+            .status(401)
+            .json({ error: error.message, expiradoEm: error.expiredAt });
+        }
         if (error) {
           return res.status(500).json({ error: error.message });
         }
