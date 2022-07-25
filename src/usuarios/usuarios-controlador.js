f18dcef30ec8c39d750a2ee91a8ea39be0dc0b6e
João Lourenço <joaodslourenco@gmail.com>
2022-07-25 10:38:08 -0300

    refactor: adjustments to match 2nd part of project course

--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -1,5 +1,6 @@
 const Usuario = require("./usuarios-modelo");
-const { InvalidArgumentError, InternalServerError } = require("../erros");
+const { InvalidArgumentError } = require("../erros");
+
 const jwt = require("jsonwebtoken");
 const blacklist = require("../../redis/manipula-blacklist");
 
@@ -7,12 +8,13 @@ function criaTokenJWT(usuario) {
   const payload = {
     id: usuario.id,
   };
-  const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "1s" });
+
+  const token = jwt.sign(payload, process.env.CHAVE_JWT, { expiresIn: "15m" });
   return token;
 }
 
 module.exports = {
-  adiciona: async (req, res) => {
+  async adiciona(req, res) {
     const { nome, email, senha } = req.body;
 
     try {
@@ -21,46 +23,51 @@ module.exports = {
         email,
       });
       await usuario.adicionaSenha(senha);
-
       await usuario.adiciona();
+
       res.status(201).json();
     } catch (erro) {
       if (erro instanceof InvalidArgumentError) {
-        res.status(422).json({ erro: erro.message });
-      } else if (erro instanceof InternalServerError) {
-        res.status(500).json({ erro: erro.message });
-      } else {
-        res.status(500).json({ erro: erro.message });
+        return res.status(400).json({ erro: erro.message });
       }
+      res.status(500).json({ erro: erro.message });
     }
   },
 
-  login: (req, res) => {
-    const token = criaTokenJWT(req.user);
-    res.set("Authorization", token);
-    res.status(204).send();
+  async login(req, res) {
+    try {
+      const token = criaTokenJWT(req.user);
+      res.set("Authorization", token);
+      res.status(204).json();
+    } catch (erro) {
+      res.status(500).json({ erro: erro.message });
+    }
   },
 
-  logout: async (req, res) => {
+  async logout(req, res) {
     try {
       const token = req.token;
       await blacklist.adiciona(token);
-      return res.status(204).send();
-    } catch (error) {
-      return res.status(500).json({ error: error.message });
+      res.status(204).json();
+    } catch (erro) {
+      res.status(500).json({ erro: erro.message });
     }
   },
 
-  lista: async (req, res) => {
-    const usuarios = await Usuario.lista();
-    res.json(usuarios);
+  async lista(req, res) {
+    try {
+      const usuarios = await Usuario.lista();
+      res.json(usuarios);
+    } catch (erro) {
+      res.status(500).json({ erro: erro.message });
+    }
   },
 
-  deleta: async (req, res) => {
-    const usuario = await Usuario.buscaPorId(req.params.id);
+  async deleta(req, res) {
     try {
+      const usuario = await Usuario.buscaPorId(req.params.id);
       await usuario.deleta();
-      res.status(200).send();
+      res.status(200).json();
     } catch (erro) {
       res.status(500).json({ erro: erro });
     }
