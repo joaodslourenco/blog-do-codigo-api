cc63f534bd7005c83a12323e7137e0b3e29e319c
João Lourenço <joaodslourenco@gmail.com>
2022-07-27 09:19:14 -0300

    feat: email is sent when new user is created

--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -1,7 +1,12 @@
 const Usuario = require("./usuarios-modelo");
 const { InvalidArgumentError } = require("../erros");
 const tokens = require("./tokens");
-const emails = require("./emails");
+const { EmailVerificacao } = require("./emails");
+
+function geraEndereco(rota, id) {
+  const baseURL = process.env.BASE_URL;
+  return `${baseURL}${rota}${id}`;
+}
 
 module.exports = {
   async adiciona(req, res) {
@@ -14,7 +19,11 @@ module.exports = {
       });
       await usuario.adicionaSenha(senha);
       await usuario.adiciona();
-      emails.enviaEmail(usuario).catch(console.log);
+
+      const endereco = geraEndereco("/usuario/verifica_email/", usuario.id);
+
+      const emailVerificacao = new EmailVerificacao(usuario, endereco);
+      emailVerificacao.enviaEmail().catch(console.log);
       res.status(201).json();
     } catch (erro) {
       if (erro instanceof InvalidArgumentError) {
