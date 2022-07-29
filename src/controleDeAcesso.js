5cefa8d8f0d88a4733bb7bc4bba8a283dbc2fccd
João Lourenço <joaodslourenco@gmail.com>
2022-07-29 11:37:15 -0300

    feat: accesscontrol to 'editor' and 'admin'

--- src/controleDeAcesso.js
+++ src/controleDeAcesso.js
@@ -5,4 +5,17 @@ controle
   .grant("assinante")
   .readAny("post", ["id", "titulo", "conteudo", "autor"]);
 
+controle
+  .grant("editor")
+  .extend("assinante")
+  .createOwn("post")
+  .deleteOwn("post");
+
+controle
+  .grant("admin")
+  .createAny("post")
+  .deleteAny("post")
+  .readAny("usuario")
+  .deleteAny("usuario");
+
 module.exports = controle;
